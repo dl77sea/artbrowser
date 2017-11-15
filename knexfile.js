6032e114851ec5bf33262c3af390966bb4925e7f
dl77sea <danlarsson77@gmail.com>
2017-11-15 15:52:31 -0800

    Prepare the Heroku

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/artbrowser_dev'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
